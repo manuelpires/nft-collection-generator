24cfb30f745428eebcd543fc85c8a91a3fb4f219
manuelpires <manuelpiresok@gmail.com>
2022-03-27 14:39:53 +0200

    Print final provenance hash on calculate-hashes script

--- scripts/calculate-hashes.js
+++ scripts/calculate-hashes.js
@@ -11,16 +11,26 @@ const {
   rmdirSync(DEFAULT_HASHES_PATH, { recursive: true });
   mkdirSync(DEFAULT_HASHES_PATH, { recursive: true });
 
+  const imagesHashes = [];
   for (let tokenId = 0; tokenId < TOTAL_TOKENS; tokenId += 1) {
     const image = readFileSync(`${DEFAULT_IMAGES_PATH}${tokenId}.png`);
     const sha256 = crypto.createHash("sha256").update(image).digest("hex");
+    imagesHashes.push(sha256);
+
     writeFileSync(
       `${DEFAULT_HASHES_PATH}${tokenId}.json`,
       JSON.stringify({ tokenId, sha256 }, null, 2)
     );
   }
 
+  const provenanceHash = crypto
+    .createHash("sha256")
+    .update(imagesHashes.join(""))
+    .digest("hex");
+
   console.log(
-    `SUCCESS! Created images SHA-256 hashes successfully at: ${DEFAULT_HASHES_PATH} directory`
+    "SUCCESS!",
+    `\n- Created images SHA-256 hashes successfully at: ${DEFAULT_HASHES_PATH} directory`,
+    `\n- Provenance hash: ${provenanceHash}`
   );
 })();
